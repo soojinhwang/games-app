4ea16b01344aa4eb32ebfe1e44504e693eeecb63
Josef Hare-Brown <josefhb@hotmail.com>
2022-05-07 16:19:36 +0100

    made base seed file for items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 5 fake video games...'
+5.times do
+  videogame = Videogame.new(
+    name:  Faker::Game.title,
+    category: "#{Faker::Game.platform}, #{Faker::Game.genre}",
+  )
+  videgame.save!
+end
+puts 'Finished!'
+
+
+puts 'Creating 5 consoles...'
+5.times do
+  console = Console.new(
+    category: Faker::Game.platform,
+  )
+  console.save!
+end
+puts 'Finished!'
+
+
+
+puts 'Creating 5 boardgames...'
+5.times do
+  boardgame = Boardgame.new(
+    name: Faker::Game.title,
+  )
+  boardgame.save!
+end
+puts 'Finished!'
+
+peripherals
+
+puts 'Creating 5 peripherals...'
+5.times do
+  peripherals = Peripherals.new(
+    name: Faker::Device.model_name,
+    category: Faker::Game.platform,
+  )
+  peripherals.save!
+end
+puts 'Finished!'
