d271231f5f570fc3634609b7cf8c618f18f8c359
Soojin Hwang <soojiny.hwang@gmail.com>
2022-05-07 16:53:10 +0100

    added validations to game, user and booking models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :games, dependet: :destroy
+  has_many :bookings, dependent: :destroy
+
+  validates :email, presence: true
+  valiadtes :password, presence: true
+  validates :username, presence: true
+
+  validates :renter, inclusion: { in: [true, false] }
+  validates :owner, inclusion: { in: [true, false] }
 end
--- db/migrate/20220507142256_create_games.rb
+++ db/migrate/20220507142256_create_games.rb
@@ -5,7 +5,7 @@ class CreateGames < ActiveRecord::Migration[6.1]
       t.string :category
       t.text :description
       t.string :location
-      t.float :price_per_day
+      t.float :price_per_day, precision: 6, scale: 2
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
