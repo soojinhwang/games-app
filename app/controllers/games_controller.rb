aa20927a6f13daa6e2bdc892a3c644ab79ab247e
Samuel Gebru <samueltgebru@gmail.com>
2022-05-10 21:44:03 +0100

    new and create controller and view completed

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,20 @@
 class GamesController < ApplicationController
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    # @game.user_id = current_user.id
+    if game.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
+
+  def game_params
+    params.require(:game).permit(:name, :category, :description, :location, :price_per_day)
+  end
 end
