dfffe11ce96807d8569555f75819692326d81a99
Samuel Gebru <samueltgebru@gmail.com>
2022-05-12 08:06:15 +0100

    add private method, change the category input on the form and add the game.user on the controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,13 +6,15 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
-    # @game.user_id = current_user.id
+    @game.user = current_user.id
     if game.save
       redirect_to game_path(@game)
     else
       render :new
     end
   end
+
+  private
 
   def game_params
     params.require(:game).permit(:name, :category, :description, :location, :price_per_day)
