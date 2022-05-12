eeb4671717f61fa03aa133c969bd06f2b4ecaef1
Samuel Gebru <samueltgebru@gmail.com>
2022-05-12 21:08:55 +0100

    remove the id from the games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -14,7 +14,7 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
-    @game.user = current_user.id
+    @game.user = current_user
     if game.save
       redirect_to game_path(@game)
     else
