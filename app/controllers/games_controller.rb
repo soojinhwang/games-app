6c79390fc0cc5a7534b93e8610c1ff48a04b88e8
Soojin Hwang <soojiny.hwang@gmail.com>
2022-05-12 22:00:13 +0100

    edit and destroy actions written in bookings controller, update needs some help

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,12 @@
 class GamesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_game, only: [ :show, :edit, :update, :destroy ]
 
   def index
     @games = Game.all
   end
 
-  def show
-    @game = Game.find(params[:id])
-  end
-
+  def show; end
 
   def new
     @game = Game.new
@@ -24,23 +22,23 @@ class GamesController < ApplicationController
     end
   end
 
-  def edit
-    @game = Game.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @game = Game.find(params[:id])
     @game.update(game_params)
     redirect_to game_path(@game)
   end
 
   def destroy
-    @game = Game.find(params[:id])
     @game.destroy
     redirect_to games_path
   end
 
   private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
 
   def game_params
     params.require(:game).permit(:name, :category, :description, :location, :price_per_day, :photo)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'pages#home'
   resources :games do
-    resources :bookings, only: [ :new, :create ]
+    resources :bookings, only: [ :index, :new, :create, :edit, :update ]
   end
-
-  resources :bookings, except: [:new, :create ]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :bookings, only: [ :show, :destroy ]
+  resources :users, only: [ :show ]
 end
