d9ff379511bef32c354c5fdd29a58aee2a913607
Samuel Gebru <samueltgebru@gmail.com>
2022-05-12 12:40:40 +0100

    implemented the logic for the update action in the games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,4 +11,16 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
   end
 
+  def update
+    @game = Game.find(params[:id])
+    @game.update(game_params)
+    redirect_to game_path(@game)
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :category, :description, :location, :price_per_day)
+  end
+
 end
