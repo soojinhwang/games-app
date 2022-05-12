f745a5adb5a8bcce3dde09b7470249c9e3085690
Samuel Gebru <samueltgebru@gmail.com>
2022-05-12 18:55:35 +0100

    new and create semi-completed for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,4 +9,43 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 
+  def new
+    @game = Game.find(params[:game_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @game = Game.find(params[:game_id])
+    @booking = Booking.new(booking_params)
+    @booking.game = @game
+
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+
+  end
+
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
+
 end
